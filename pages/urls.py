6b2d3704d6a84fc534280afd824a487e0d849f0f
Nils <olivernils8@gmail.com>
2023-01-09 11:06:52 +0100

    [ADD] Added index, ticket and card detail page skeleton

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import main_page
+from .views import main_page, card_detail, ticket_page
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', main_page),
+    path('problem', card_detail),
+    path('ticket', ticket_page),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def main_page(request):
 	return render(request, "index.html")
+
+def card_detail(request):
+	return render(request, "card-detail.html")
+
+def ticket_page(request):
+	return render(request, "ticket.html")
