f9a751c94423322a34497e0d886c0a7784ea9081
Nils <olivernils8@gmail.com>
2023-02-03 15:34:22 +0100

    [SYNC]

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Ticket:
+    emne = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    detalj = models.TextField(max_length=1000)
+
+    oppdragstaker
