c36bd740da8cc06ad1c86b382e54a9118ff983ef
Nils <olivernils8@gmail.com>
2023-02-08 10:21:30 +0100

    [CHANGE] Removed helpdesk app

--- helpdesk/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Ticket:
-    emne = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
-    detalj = models.TextField(max_length=1000)
-
-    oppdragstaker
